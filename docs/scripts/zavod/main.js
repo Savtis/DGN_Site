5a16163fe9cc83a4f11e456136e636d1fd0d8113
Savtis <nihyisebe@gmail.com>
2024-11-29 02:47:48 +0700

    последний коммит в этом году

--- docs/scripts/zavod/main.js
+++ docs/scripts/zavod/main.js
@@ -1,5 +1,12 @@
 load_sounds();
 
+let i = 0;
+
 document.addEventListener('click', function () {
-    get_sound('metal').play_random();
+    get_sound(sound_packs_names[i]).play_random();
+    if (i >= 2) {
+        i = 0;
+    } else {
+        i++;
+    }
 });
---
+++ docs/шаблоны/win.js
@@ -0,0 +1,5 @@
+const img = document.createElement('img');
+img.src = 'res/engi.png';
+img.alt = 'lol';
+img.className = 'fixed-image';
+document.body.appendChild(img);
